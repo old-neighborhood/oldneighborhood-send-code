fa57db8466fa8fa0bef4698caf51cb2d35fb0022
RenBrian <Brian_Ren@outlook.com>
2018-03-21 17:18:13 +0800

    "springboot start"

---
+++ oldneighborhood-send-code/src/main/java/com/oldneighborhood/demo/OldneighborhoodSendCodeApplication.java
@@ -0,0 +1,12 @@
+package com.oldneighborhood.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OldneighborhoodSendCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OldneighborhoodSendCodeApplication.class, args);
+	}
+}
