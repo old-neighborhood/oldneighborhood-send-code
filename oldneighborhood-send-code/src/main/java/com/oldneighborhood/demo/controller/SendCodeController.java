67306045940b6d51019e100e49cf8eb61b0dc0fc
lzy141250084 <141250084@smail.nju.edu.cn>
2018-03-23 09:52:25 +0800

    测试

--- oldneighborhood-send-code/src/main/java/com/oldneighborhood/demo/controller/SendCodeController.java
+++ oldneighborhood-send-code/src/main/java/com/oldneighborhood/demo/controller/SendCodeController.java
@@ -22,23 +22,27 @@ public class SendCodeController {
 	
 	private static final String KEY = "abc123";
 	
+	
+//	传hashMap 里面有phonenumber；return一个resultMap 里面有五分钟后时间、MD5码
 	@RequestMapping(value = "/sendMsg", method = RequestMethod.POST, headers = "Accept=application/json")
 	public Map<String, Object> sendMsg(@RequestBody Map<String,Object> requestMap) {
 	String phoneNumber = requestMap.get("phoneNumber").toString();
 	String randomNum = CommonUtils.createRandomNum();// 生成随机数
+	System.out.println(randomNum);
 	SimpleDateFormat sf = new SimpleDateFormat("yyyyMMddHHmmss ");
 	Calendar c = Calendar.getInstance();
 	c.add(Calendar.MINUTE, 5);
 	String currentTime = sf.format(c.getTime());// 生成5分钟后时间，用户校验是否过期
-	SendMsg.sendMsg(phoneNumber,randomNum); //此处执行发送短信验证码方法
+//	SendMsg.sendMsg(phoneNumber,randomNum); //此处执行发送短信验证码方法
 	String hash = MD5Utils.getMD5Code(KEY + "@" + currentTime + "@" + randomNum);//生成MD5值
 	Map<String, Object> resultMap = new HashMap<>();
 	resultMap.put("hash", hash);
 	resultMap.put("tamp", currentTime);
+//	resultMap.put("randomNum", randomNum);
 	return resultMap; //将hash值和tamp时间返回给前端
 }
 	
-	
+//	传resultMap（界面put验证码“msgNum”）；return 0 成功 1 失败；2 超时
 	@RequestMapping(value = "/validateNum", method = RequestMethod.POST, headers = "Accept=application/json")
 	public int validateNum(@RequestBody Map<String,Object> requestMap) {
 	String requestHash = requestMap.get("hash").toString();
--- oldneighborhood-send-code/src/test/java/com/oldneighborhood/demo/OldneighborhoodSendCodeApplicationTests.java
+++ oldneighborhood-send-code/src/test/java/com/oldneighborhood/demo/OldneighborhoodSendCodeApplicationTests.java
@@ -1,9 +1,14 @@
 package com.oldneighborhood.demo;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.oldneighborhood.demo.controller.SendCodeController;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,6 +16,12 @@ public class OldneighborhoodSendCodeApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		SendCodeController sendCodeController = new SendCodeController();
+		Map<String,Object> requestMap = new HashMap<>();
+		String phoneNumber = "18260099137";
+		requestMap.put("phoneNumber", phoneNumber);
+		System.out.println(sendCodeController.sendMsg(requestMap));
+		
 	}
 
 }
